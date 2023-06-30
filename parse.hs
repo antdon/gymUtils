752f4850466798ddb8ba721eb08cafaa6dc62e27
antdon <a.donohue@uqconnect.edu.au>
2023-06-30 17:33:05 +1000

    parser

---
+++ parse.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+import Text.Parsec
+import Text.Parsec.String
+import Control.Applicative ((<$>), (<*>), (<*), (*>))
+import Control.Monad.Identity
+import Debug.Trace
+import Data.Aeson
+import GHC.Generics
+import Data.ByteString.Lazy  
+
+data Workout = Workout {
+    date :: Maybe String,
+    workoutType :: Maybe String,
+    exercises :: [Exercise]
+} deriving (Show, Generic)
+
+
+data Exercise = Exercise {
+  name :: String,
+  reps :: Int,
+  sets :: Int,
+  weight :: Maybe Double,
+  failure :: Maybe Bool,
+  improvement :: Maybe Bool
+} deriving (Show, Generic)
+
+-- The default definitions of the methods are defined in terms
+-- of the methods provided by the Generic type class.
+instance ToJSON Exercise where
+    toEncoding = genericToEncoding defaultOptions
+instance FromJSON Exercise
+instance ToJSON Workout where
+    toEncoding = genericToEncoding defaultOptions
+instance FromJSON Workout
+
+workoutFile = option [] (sepEndBy workout (many1 endOfLine)) <* eof
+workout = do
+  trace ("reading workout") $ return ()
+  skipMany (satisfy (/= '\n')) -- Skip over any characters that are not end-of-line characters
+  endOfLine
+  date <- optionMaybe $ try $ manyTill anyChar endOfLine
+  trace ("date: " ++ show date) $ return ()
+  workoutType <- optionMaybe (try (string "Push") <|> try (string "Pull") <|> string "Legs")
+  skipMany (satisfy (/= '\n')) -- Skip over any characters that are not end-of-line characters
+  endOfLine
+  trace ("workoutType: " ++ show workoutType) $ return ()
+  exercises <- option [] $ manyTill exercise (lookAhead (try endOfLine <|> (try eof >> return ' ')))
+  ---- trace exercises
+  ----trace ("exercises: " ++ show exercises) $ return ()
+  return $ Workout date workoutType exercises
+
+exercise = do
+    name <- manyTill anyChar (lookAhead digit)
+    reps <- read <$> many1 digit
+    _ <- char ' '
+    sets <- read <$> many1 digit
+    _ <- char ' '
+    weight <- char '-' *> return Nothing <|> (Just . read <$> many1 (digit <|> char '.'))
+    _ <- char ' '
+    failure <- optionMaybe (char 'y' *> return True <|> char 'n' *> return False <|> char '-' *> return False)
+    _ <- char ' '
+    improvement <- optionMaybe (char 'y' *> return True <|> char 'n' *> return False <|> char '-' *> return False)
+    skipMany (char ' ')
+    _ <- endOfLine
+    return $ Exercise name reps sets weight failure improvement
+
+main :: IO ()
+main = do
+    contents <- Prelude.readFile "data.txt"
+    case parse workoutFile "" contents of
+        Left err -> putStrLn $ "Parsing error: " ++ show err
+        Right workouts -> do
+            putStrLn "Parsed workouts:"
+            Data.ByteString.Lazy.writeFile "output.json" (encode workouts)
+
+
